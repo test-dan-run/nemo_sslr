a3a29c633882e669ac29659477f71220d350d33e
test-dan-run <daniel.leongsj@gmail.com>
2022-02-24 09:57:57 +0800

    add citrinet config

--- pretrain.py
+++ pretrain.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 from clearml import Task, Dataset
 
-task = Task.init(project_name='nemo_sslr', task_name='conformer_large_pretrain_id_only', output_uri='s3://experiment-logging/storage', task_type='training')
+task = Task.init(project_name='nemo_sslr', task_name='citrinet_1024_pretrain_id_only', output_uri='s3://experiment-logging/storage', task_type='training')
 task.add_tags(['NeMo Toolkit', 'Pretraining', 'ASR', 'Voxlingua'])
 task.set_base_docker('dleongsh/nemo_asr:c022f029')
 task.execute_remotely(queue_name='compute', clone=False, exit_process=True)
@@ -54,7 +54,7 @@ When doing supervised fine-tuning from unsupervised pre-trained encoder, set fla
 """
 
 
-@hydra_runner(config_path="./configs", config_name="large_id_sslr")
+@hydra_runner(config_path="./configs", config_name="citrinet_1024_id_sslr")
 def main(cfg):
     logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
 
